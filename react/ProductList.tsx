5941a843310044a1adf32286934eec43e91a8e08
Christian G <cdavid.ggarcia@gmail.com>
2022-01-19 14:46:55 +0100

    fix: remove extra slash in url

--- react/ProductList.tsx
+++ react/ProductList.tsx
@@ -19,10 +19,12 @@ export function getProductList(products?: Product[]) {
     return null
   }
 
-  const baseUrl = getBaseUrl()
-
+  const baseUrl= getBaseUrl()
+  const slash = baseUrl && baseUrl?.slice(-1) === '/' ? '': '/'
   const productItems: ListItem[] = products.map((product, index) => {
-    const productUrl = `${baseUrl}/${product.link ?? `${product.linkText}/p`}`
+    let link = product.link ?? `${product.linkText}/p`
+    link = link.slice(0, 1) === '/' ? link.substring(1) : link
+    const productUrl = `${baseUrl}${slash}${link}`
     return {
       '@type': 'ListItem',
       position: index + 1,
