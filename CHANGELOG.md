9b85d20adb680fc703915cae7dea8bc6a94adb7d
Jorge Acosta <jorge.acosta@vtex.com>
2024-03-08 17:40:16 -0500

    Remove SSR settings

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+### Fixed
+
+- SSR settings remove for disable offers
+
 ## [0.12.0] - 2023-05-10
 
 ### Added
--- react/hooks/useAppSettings.ts
+++ react/hooks/useAppSettings.ts
@@ -15,7 +15,7 @@ interface Settings {
 }
 
 const useAppSettings = (): Settings => {
-  const { data } = useQuery(GET_SETTINGS, { ssr: false })
+  const { data } = useQuery(GET_SETTINGS, {})
 
   if (data?.publicSettingsForApp?.message) {
     const {
